8820f9b57806332f45a5b7a123b744f5fde077f1
Tolga1452 <tolgchu-studios@outlook.com>
2023-04-18 19:21:47 +0300

    fix test

--- __tests__/shuffle.test.ts
+++ __tests__/shuffle.test.ts
@@ -1,13 +1,6 @@
 import { shuffle } from '../';
 
 describe('shuffle', () => {
-    it('should shuffle the given array', () => {
-        const arr = ['red', 'green', 'blue'];
-        const expected = ['red', 'green', 'blue'];
-
-        expect(shuffle(arr)).not.toEqual(expected);
-    });
-
     it('should work with arrays of different types', () => {
         const strArr = ['hello', 'world'];
         const numArr = [1, 2, 3];
