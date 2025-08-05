b3160c17ad2f38a7057708adfacfb2d11e536e0e
✨Tolgchu✨ <tolgchu-studios@outlook.com>
2025-08-05 15:59:25 +0300

    ??

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,9 +2,9 @@ import { TimeUnit } from '../types/time.js';
 
 export const HEX_FORMAT = /^#(?:[A-Fa-f0-9]{3}|[A-Fa-f0-9]{4}|[A-Fa-f0-9]{6}|[A-Fa-f0-9]{8})$/;
 
-export const TIME_STRING_FORMAT = /^\s*[+-]?\d+(?:\.\d+)?\s*(?:milliseconds?|msecs?|ms|years?|yrs?|y|weeks?|wks?|w|days?|d|hours?|hrs?|h|minutes?|mins?|min|m|seconds?|secs?|sec|s)\b(?:\s+(?:,|\band\b)?\s*[+-]?\d+(?:\.\d+)?\s*(?:milliseconds?|msecs?|ms|years?|yrs?|y|weeks?|wks?|w|days?|d|hours?|hrs?|h|minutes?|mins?|min|m|seconds?|secs?|sec|s)\b)*\s*$/i;
+export const TIME_STRING_FORMAT = /^\s*(?:[+-]?\d+(?:\.\d+)?\s*(?:milliseconds?|msecs?|ms|years?|yrs?|y|weeks?|wks?|w|days?|d|hours?|hrs?|h|minutes?|mins?|min|m|seconds?|secs?|sec|s))(?:\s*(?:,|\band\b)?\s*[+-]?\d+(?:\.\d+)?\s*(?:milliseconds?|msecs?|ms|years?|yrs?|y|weeks?|wks?|w|days?|d|hours?|hrs?|h|minutes?|mins?|min|m|seconds?|secs?|sec|s))*\s*$/i;
 
-export const TIME_UNIT_FORMAT = /([+-]?\d+(?:\.\d+)?)[ \t]{0,64}(?:(?:milliseconds?|minutes?|msecs?|mins?|min|ms|m)|(?:years?|yrs?|y)|(?:weeks?|wks?|w)|(?:days?|d)|(?:hours?|hrs?|h)|(?:seconds?|secs?|sec|s))\b/gi;
+export const TIME_UNIT_FORMAT = /([+-]?\d+(?:\.\d+)?)\s*(milliseconds?|msecs?|ms|years?|yrs?|y|weeks?|wks?|w|days?|d|hours?|hrs?|h|minutes?|mins?|min|m|seconds?|secs?|sec|s)/gi;
 
 export const TIME_UNIT_MAP: Map<string, TimeUnit> = new Map([
     ['ms', TimeUnit.Millisecond], ['msec', TimeUnit.Millisecond], ['msecs', TimeUnit.Millisecond], ['millisecond', TimeUnit.Millisecond], ['milliseconds', TimeUnit.Millisecond],
